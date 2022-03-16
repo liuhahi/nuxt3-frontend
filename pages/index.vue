1249cee77da202446514441a97b4a8029b0017bc
christopherkekzijing <chrisartbp@gmail.com>
2022-03-16 14:40:55 +0800

    edit

--- pages/index.vue
+++ pages/index.vue
@@ -16,15 +16,12 @@
     </div>
   </div>
 </template>
-<script setup>
-  import 'element-plus/dist/index.css';
-  // const { data } = await useFetch("api/hello");   
+<script>
 </script>
 <style scoped>
-/* @import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap'); */
 .background {
-  width: 100vw;
-  height: 100vh;
+  /* width: 100%;
+  height: 100%; */
   padding: 1rem;
   background: linear-gradient(to left, #03d7bb, #066f97);
 }
